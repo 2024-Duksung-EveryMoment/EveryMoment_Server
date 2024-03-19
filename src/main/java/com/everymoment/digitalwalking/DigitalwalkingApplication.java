eafcbe23dad30284994c29589fe54b7eb1089115
hyeonji0730 <leehyeonji0730@naver.com>
2024-03-20 00:58:47 +0900

    project structure

--- src/main/java/com/everymoment/digitalwalking/DigitalwalkingApplication.java
+++ src/main/java/com/everymoment/digitalwalking/DigitalwalkingApplication.java
@@ -2,8 +2,9 @@ package com.everymoment.digitalwalking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class DigitalwalkingApplication {
 
 	public static void main(String[] args) {
