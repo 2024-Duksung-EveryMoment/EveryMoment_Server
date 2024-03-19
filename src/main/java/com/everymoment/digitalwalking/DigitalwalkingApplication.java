f0ee0ce77c6f730c18a5e9b3ee30626b561099e3
hyeonji0730 <leehyeonji0730@naver.com>
2024-03-19 23:26:46 +0900

    project file

---
+++ src/main/java/com/everymoment/digitalwalking/DigitalwalkingApplication.java
@@ -0,0 +1,13 @@
+package com.everymoment.digitalwalking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalwalkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalwalkingApplication.class, args);
+	}
+
+}
