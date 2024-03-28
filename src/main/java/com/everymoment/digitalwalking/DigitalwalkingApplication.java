9e15891af3058d60805a026cad1b56c33ce2c095
hyeonji0730 <leehyeonji0730@naver.com>
2024-03-29 02:10:37 +0900

    db connect

--- src/main/java/com/everymoment/digitalwalking/DigitalwalkingApplication.java
+++ src/main/java/com/everymoment/digitalwalking/DigitalwalkingApplication.java
@@ -3,7 +3,9 @@ package com.everymoment.digitalwalking;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+@EnableMongoRepositories
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class DigitalwalkingApplication {
 
